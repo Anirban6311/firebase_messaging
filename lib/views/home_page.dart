bfd04a7917b5150371523841e373cbc54faa88fd
Anirban Sinha Roy <107030944+Anirban6311@users.noreply.github.com>
2025-01-17 18:30:48 +0530

    Update home_page.dart

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../controllers/auth_service.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -10,6 +12,31 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("Home Page")),
+        backgroundColor: Colors.greenAccent,
+        actions: [
+          ElevatedButton(
+              onPressed: () {
+                AuthService.logout().then((val) {
+                  try {
+                    if (val == "Logged Out") {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Logged Out Successfully")));
+                      Navigator.pushNamedAndRemoveUntil(
+                          context, "/login", (route) => false);
+                    }
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(e.toString())),
+                    );
+                  }
+                });
+              },
+              child: const Icon(Icons.logout))
+        ],
+      ),
+    );
   }
 }
